ec9b08d5ffd3dd648dd863c445f69942f12866e9
michalkurzeja <mike.kurzeja@gmail.com>
2015-04-22 20:18:41 +0200

    code finalization and cleanup

--- src/serverApi.pl
+++ src/serverApi.pl
@@ -1,57 +1,40 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_client)).
-:- use_module(library(http/http_error)).
-:- use_module(library(http/http_parameters)).
-:- use_module(library(threadutil)).
-:- use_module(library(http/json)).
-:- use_module(library(http/json_convert)).
-:- use_module(library(http/http_json)).
-:- use_module(library(debug)).
 
 :- use_module(requestDataParser).
-
-:- json_object jRequest(param:text).
 
 %% Routing:
 :- http_handler(root(.), getRequest, []).
 
-%% Action handlers
+%% Server init - entry point to application
+start :- runServer(9147).
 
-% INFO
-handleRequest('INFO', Response) :-
-	Response = 'AVAILABLE'.
+%% Action handlers (currently doing nothing)
+handleRequest('INFO', _, Response) :- Response = 'available'.
+handleRequest('START', _, Response) :- Response = 'ready'.
+handleRequest('PLAY', _, Response) :- Response = 'nil'.
+handleRequest('PLAY', _, Response) :- Response = 'ready'.
+handleRequest('ABORT', _, Response) :- Response = 'aborted'.
+
+% Do we want to handle this case?
+handleRequest('PREVIEW', _, Response) :- Response = 'done'.
 
 % Unknown data
-handleRequest(Data, Response) :-
-	%% trace,
-	atom(Data),
-	Response = 'READY'.
+handleRequest(_, _, Response) :- Response = 'nil'.
 
 %% Request handlers:
 getRequest(Request) :-
-	http_read_data(Request, Data, []),
-	stripParenthesis(Data, ExtractedData),
-	handleRequest(ExtractedData, Response),
-    format('Content-type: text/acl~n~n', []),
+	http_read_data(Request, RequestData, []),
+	requestDataParser:parseRequest(RequestData, Action, Data),
+	handleRequest(Action, Data, Response),
+	formatResponse(Response).
+    
+formatResponse(Response) :-
+	format('Content-type: text/acl~n~n'),
     format(Response).
-
-%% Data string operations
-stripParenthesis(Data, Stripped) :-
-	sub_string(Data, 2, _, 2, Substring),
-	atom_string(Stripped, Substring).
-
-%% Server init
-start :-
-	enableDebug,	
-	runServer(9147).
 
 runServer(Port) :-
 	format('~nStarting player...~n'),
 	http_server(http_dispatch, [port(Port)]),
 	format('Player ready!~n~n').
-
-%% Debugging hooks
-enableDebug :-
-	debug(http(request)),
-	tspy(handleRequest/2).
